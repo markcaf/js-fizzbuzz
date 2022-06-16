8a3dc6777c8034bb18cda0bbeb833fbc5b324294
markcaf <marcocafassog@gmail.com>
2022-06-16 17:48:43 +0200

    Added the variable that creates an empty div

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
+// Dichiaro la variabile della row che sarà utilizzata come container 
+// della funzione append per visualizzare gli elementi in html
+const rowNumberWrapper = document.getElementById("row_number_wrapper");
+
 // Creo un ciclo for per eseguire la stampa dei numeri da 1 a 100
 for (let i = 1; i <= 100; i++ ){
+    // Creo una variabile che crea un div vuoto
+    const msBox = document.createElement("div");
     // Aggiungo le condizioni per multipli di 3 e 5
     if ((i % 3 == 0) && (i % 5 == 0)){
             console.log("FizzBuzz");
