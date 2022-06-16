361356db06f65aa8f28f1ec8a5884ca4bb6bd8b0
markcaf <marcocafassog@gmail.com>
2022-06-16 17:51:10 +0200

    Added classes col-2 and ms_box to msBox empty div

--- js/script.js
+++ js/script.js
@@ -4,8 +4,13 @@ const rowNumberWrapper = document.getElementById("row_number_wrapper");
 
 // Creo un ciclo for per eseguire la stampa dei numeri da 1 a 100
 for (let i = 1; i <= 100; i++ ){
+    
     // Creo una variabile che crea un div vuoto
     const msBox = document.createElement("div");
+
+    // Aggiungo al div vuoto le classi col-2 e ms_box
+    msBox.classList.add("col-2", "ms_box");
+
     // Aggiungo le condizioni per multipli di 3 e 5
     if ((i % 3 == 0) && (i % 5 == 0)){
             console.log("FizzBuzz");
