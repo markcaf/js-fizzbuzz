7bbe60f0170e3112df77770c8c9ba5280d124013
markcaf <marcocafassog@gmail.com>
2022-06-16 17:02:05 +0200

    Output console completed with Fizz Buzz conditions

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 // Creo un ciclo for per eseguire la stampa dei numeri da 1 a 100
 for (let i = 1; i <= 100; i++ ){
-    console.log(i);
+    // Aggiungo le condizioni per multipli di 3 e 5
+    if ((i % 3 == 0) && (i % 5 == 0)){
+            console.log("FizzBuzz");
+        } else if(i % 3 == 0){
+                console.log("Fizz");
+            } else if(i % 5 == 0){
+                    console.log("Buzz");
+                } else{
+                        console.log(i);
+                    }
 }
